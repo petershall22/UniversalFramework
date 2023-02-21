1bbe13d11c53c9096d63839f6b87b5a9c60f1ab4
Peter Lane <peterrbxl@gmail.com>
2023-02-21 23:11:27 +0000

    NotificationService

--- src/StarterGui/NotificationService/Main.client.lua
+++ src/StarterGui/NotificationService/Main.client.lua
@@ -2,16 +2,47 @@ local UniversalFramework = game:GetService("ReplicatedStorage"):WaitForChild("Un
 local TweenService = game:GetService("TweenService")
 local Knit = require(UniversalFramework.Utility.KnitFramework.Knit)
 local Utils = require(UniversalFramework.Utility.FrameworkUtils)
-local Systems = UniversalFramework.Systems
-local Types = require(Systems.NotificationSystem.Types)
+local Types = require(UniversalFramework.Configuration.NotificationService.Types)
 
 local notificationService = Knit.GetService("NotificationService")
+local gui = script.Parent.MainFrame
 
-local gui = script.Parent
+-- Information about the tween
+local notificationTweenInfo = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut)
+local startPosX = 1.2
+local holdingTime = 5
+--
 
-notificationService.CreateNotification:Connect(function(player, status, message)
+notificationService.CreateNotification:Connect(function(status, title, message)
+	local posY = gui.Notif.Body.Position.Y.Scale
+	local endPosX = gui.Notif.Body.Position.X.Scale
+	local startPos = UDim2.new(startPosX, 0, posY, 0)
+	local endGoal = UDim2.new(endPosX, 0, posY, 0)
+
 	local types = Types.returnTypes()
-	local clone = gui.Template:Clone()
-	clone.Status.BackgroundColor = types[status]["Color"]
-	clone.Status.Status.Text = status
+	local clone = gui.Notif:Clone()
+
+	clone.Body.Position = startPos
+	clone.Body.StatusImage.ImageColor3 = types[status]["Color"]
+	clone.Body.Status.BackgroundColor3 = types[status]["Color"]
+	clone.Body.Status.Misc.BackgroundColor3 = types[status]["Color"]
+	clone.Body.StatusImage.Image = types[status]["Image"]
+	clone.Notification.SoundId = types[status]["Sound"]
+	clone.Body.Status.Status.Text = title
+	clone.Body.Body.Text = message
+	clone.Visible = true
+	clone.Parent = gui
+
+	local tweenIn = TweenService:Create(clone.Body, notificationTweenInfo, { Position = endGoal })
+	tweenIn:Play()
+	clone.Notification:Play()
+	tweenIn.Completed:Connect(function()
+		Utils.Hold(holdingTime)
+		local tweenOut = TweenService:Create(clone.Body, notificationTweenInfo, { Position = startPos })
+		tweenOut:Play()
+		tweenOut.Completed:Connect(function()
+			clone:Destroy()
+		end)
+	end)
+
 end)
